11b2d13c5ed1977dbc775401daacfff69e068d6d
Charles Marques <charles_sjp@yahoo.com.br>
2022-06-23 21:04:58 -0300

    implement swagger

--- src/game/dto/create-game-dto.ts
+++ src/game/dto/create-game-dto.ts
@@ -1,8 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsPositive } from "class-validator";
 
 export class CreateGameDto{
   @IsNumber()
   @IsPositive()
+  @ApiProperty({
+    description: 'O nº do game',
+    example: 1,
+  })
   number: number;
   name:string;
 };
--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { CreateGameDto } from "./dto/create-game-dto";
 import { GameService } from "./game.service";
+import { ApiTags } from "@nestjs/swagger";
 
+@ApiTags('game')
 @Controller('game')
 export class GameController {
   constructor (private gameService: GameService) {}
