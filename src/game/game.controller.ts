d702b5274f49d466908fbf72fa3399aca01fd76a
Charles Marques <charles_sjp@yahoo.com.br>
2022-07-02 16:06:09 -0300

    implement findOne at  gameController

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,17 +1,34 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { CreateGameDto } from "./dto/create-game-dto";
 import { GameService } from "./game.service";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Game } from "./entities/game.entity";
 
 @ApiTags('game')
 @Controller('game')
 export class GameController {
   constructor (private gameService: GameService) {}
+
   @Get()
-  findAll() {
+  @ApiOperation({
+    summary: 'Listar todos os games!'
+  })
+  findAll(): Promise<Game[]> {
     return this.gameService.findAll();
   }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Visualizar um game!'
+  })
+  findOne(@Param('id') id: string): Promise<Game>{
+    return this.gameService.findOne(id)
+  }
+
   @Post()
+  @ApiOperation({
+    summary: 'Criar um game!'
+  })
   create(@Body() dto: CreateGameDto) {
     return this.gameService.create(dto);
   }
