c45e4a2a75649a40104222b5f4b2f8c2416e374e
Charles Marques <charles_sjp@yahoo.com.br>
2022-07-08 21:59:27 -0300

    feat(profile) implement crud resoucrce

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { ProfileModule } from './profile/profile.module';
 
 
 @Module({
-  imports: [GameModule, PrismaModule, UsersModule],
+  imports: [GameModule, PrismaModule, UsersModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
